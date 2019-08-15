e515c694f33aaac40a27ed88fb67f375a68360f1
Jamon Holmgren <jamon@infinite.red>
2019-08-15 10:31:26 -0700

    Re-export react-navigation

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import * as storageX from "./storage/storage"
 export const storage = storageX
 export * from "mobx-react"
 export * from "mobx-state-tree"
+export * from "react-navigation"
 export * from "./stores/navigation-store"
 export * from "./stores/use-stores"
 export * from "./navx"
